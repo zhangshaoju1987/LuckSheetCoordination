cde3e8da9d89870a74fdc569e180fbfad5290161
zhangshaoju1987 <69586325+zhangshaoju1987@users.noreply.github.com>
2022-07-03 16:50:54 +0800

    初始化Lucksheet协同模块

---
+++ config.ts
@@ -0,0 +1,18 @@
+export default  {
+    redis:{
+        channel:"redis.channel" ,
+        connection:{
+
+        },
+        pool:{
+
+        }
+    },
+    mongodb:{
+        uri:""
+    },
+    sheet:{
+        row_size:500,
+        col_size:500
+    }
+}
